d7582eb43f9f073775da27713a44a1f0e2225cc3
Johns Gresham <johnsgresham@Johnss-MacBook-Pro.local>
2017-09-18 17:01:21 -0700

    paid

--- assets/js/gmap.js
+++ assets/js/gmap.js
@@ -6,7 +6,7 @@
 
 $(document).ready(function(){
 
-	
+
 	function initGMap() {
 		'use strict';
 
@@ -16,7 +16,7 @@ $(document).ready(function(){
 		scrollwheel: false,
 
 		// The latitude and longitude to center the map (always required)
-		center: new google.maps.LatLng(41.880038, -87.601127), // Chicago
+		center: new google.maps.LatLng(37.4419, -122.1430), // Palo Alto
 
 		// Styling of the map in JSON object
 		styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#cdd2d4"},{"visibility":"on"}]}]
@@ -24,7 +24,7 @@ $(document).ready(function(){
 		var map = new google.maps.Map(document.getElementById('map-canvas'),
 		  mapOptions);
 
-		// Map marker 
+		// Map marker
 		var image = '/assets/images/map-pin.png';
 		var myLatLng = new google.maps.LatLng(41.880038, -87.631127);
 		var beachMarker = new google.maps.Marker({
